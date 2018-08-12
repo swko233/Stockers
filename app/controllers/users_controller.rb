b60c0369c6891b4a109a6fc2931e135bb6f0b235
swko233 <swkon233@gmail.com>
2018-08-12 11:08:50 +0000

    add_follow/follower-index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,16 @@ class UsersController < ApplicationController
 	def show
 		@user = User.find(params[:id])
 	end
+
+	def following
+		@user = User.find(params[:id])
+		@users = @user.following
+		render 'show_following'
+	end
+
+	def followers
+		@user = User.find(params[:id])
+		@users = @user.followers
+		render 'show_followers'
+	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :following_relationships, foreign_key: "follower_id", class_name: "Relationship", dependent: :destroy
-  has_many :followings, through: :following_relationships
+  has_many :following, through: :following_relationships
   has_many :follower_relationships, foreign_key: "following_id", class_name: "Relationship", dependent: :destroy
   has_many :followers, through: :follower_relationships
 
