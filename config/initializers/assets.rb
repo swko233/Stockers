6a13becad55390a2ee94ad938df98a3abdb6abaf
swko233 <swkon233@gmail.com>
2018-08-13 09:06:33 +0000

    modify_scss-apply

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -14,3 +14,6 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 
 Rails.application.config.assets.precompile += %w( bookmarks.js )
+
+Rails.application.config.assets.precompile += %w( bookmarks/new.scss )
+Rails.application.config.assets.precompile += %w( users/show.scss )
