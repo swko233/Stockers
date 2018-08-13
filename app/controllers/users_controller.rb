b85569aa18909c10cabdfe8863817eea658c3188
swko233 <swkon233@gmail.com>
2018-08-13 08:51:26 +0000

    modify_user-mypage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,15 @@ class UsersController < ApplicationController
 		@user = User.find(params[:id])
 	end
 
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	# def update
+	# 	@user = User.find(params[:id])
+	# 	if @user.save(user_params)
+	# end
+
 	def following
 		@user = User.find(params[:id])
 		@users = @user.following
