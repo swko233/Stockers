4b301908c53fde3d7893830f9641b64aeb7c4429
swko233 <swkon233@gmail.com>
2018-08-17 17:14:37 +0000

    modify_user_image

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+50.times do |n|
+	email = 'seed' + n + '@gmail.com'
+	user_name = 'user' + n
+	nickname = Faker::Overwatch.hero
+	image_number = rand(6..10)
+	User.create!(email: email,
+				password: '000000',
+				name: user_name,
+				nickname: nickname,
+				product_image_name: File.open("./app/assets/images/#{image_number}.jpg")
+				)
+end
