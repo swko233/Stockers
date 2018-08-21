cda78656a8c0ddaf4bbbd12c5579da063aa0c8e2
swko233 <swkon233@gmail.com>
2018-08-21 16:55:19 +0000

    modify_header_menu

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,7 +18,32 @@
 //= require bootstrap-sprockets
 //= require bootstrap-tagsinput.min
 
-// バリデーションエラー時の「必須」アイコンのずれ解消
-$(function(){
-	$("div.field_with_errors:has(label)").addClass("parent");
+// ブラウザバック時にドロップダウンが開けなかったので対策
+$(document).on('turbolinks:load',function(){
+	// バリデーションエラー時の「必須」アイコンのずれ解消
+	$(function(){
+		$("div.field_with_errors:has(label)").addClass("parent");
+	});
+	// ヘッダーのドロップダウンメニュー開閉
+	$(function(){
+		//ドロップダウンを開く
+		$('.user-menu').on('click', function(){
+			// 下の処理で、メニュー非表示のためにユーザー情報欄をクリックしてもイベントが起きなくなったので追記
+			if ($('#dropdown-window').hasClass('is-active')){
+				$('#dropdown-window').removeClass('is-active');
+			}else{
+				$('#dropdown-window').addClass('is-active');
+			}
+		});
+		// ドロップダウンを閉じる
+		$(document).on('click',function(e){
+			// ドロップダウンメニューを表示させるためのクリックで発火するのを防ぐ（ユーザー情報欄を除外する）
+			if (!$(e.target).is('.user-menu, .user-menu *, #dropdown-window, #dropdown-window *')){
+				// alert();
+				if ($('#dropdown-window').hasClass('is-active')){
+					$('#dropdown-window').removeClass('is-active');
+				}
+			}
+		});
+	});
 });
